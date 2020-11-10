42873cd025190aa987b90776bc798d344215f60a
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-10 17:11:25 -0500

    Add edit method and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,14 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to new_user_path
+  end
 end
